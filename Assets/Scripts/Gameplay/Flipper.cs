2d134dc89ac4d25a991335727df2570897c93e12
karol.marszalik@gmail.com <>
2023-02-03 22:39:51 +0100

    Added flipping by doTween

--- Assets/Scripts/Gameplay/Flipper.cs
+++ Assets/Scripts/Gameplay/Flipper.cs
@@ -1,24 +1,31 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Flipper : MonoBehaviour
 {
+    [SerializeField]
+    private float _rotationDuration = 0.5f;
+
+    private bool _rotationInProgress;
     private int _currentRotationIndex;
     private FlipperRotationValue[] _flipperRotationValues =
         { new(0, 0, -45), new(0, 0, 45) };
 
     private void OnMouseOver()
     {
-        if (Input.GetMouseButton(0))
-        {
-            SetRotationIndex();
-            FlipRotation();
-        }
+        if (!Input.GetMouseButton(0)) return;
+        if (_rotationInProgress) { return; }
+
+        _rotationInProgress = true;
+        SetRotationIndex();
+        FlipRotation();
+        
     }
 
     private void SetRotationIndex()
     {
         _currentRotationIndex++;
-        if (_currentRotationIndex > _flipperRotationValues.Length)
+        if (_currentRotationIndex >= _flipperRotationValues.Length)
         {
             _currentRotationIndex = 0;
         }
@@ -26,7 +33,7 @@ public class Flipper : MonoBehaviour
 
     private void FlipRotation()
     {
-        //Add DoTween
+        transform.DORotate(_flipperRotationValues[_currentRotationIndex].FlipperRotation,_rotationDuration).OnComplete(() => _rotationInProgress = false);
     }
 }
 
